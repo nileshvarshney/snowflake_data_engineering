4e592fdbbfe53f5d07c118e236743d37fb829c3b
varshney-nilesh <varshney_nilesh@yahoo.com>
2025-04-27 20:52:19 -0700

    initial

--- snowflake_devops/02-create_task.py
+++ snowflake_devops/02-create_task.py
@@ -3,6 +3,7 @@ from snowflake.snowpark import Session
 from snowflake.core import Root, CreateMode
 from snowflake.snowpark.functions import col
 from snowflake.core.task import Task, StoredProcedureCall
+import time
 
 
 DATABASE_NAME = "python_api_db"
@@ -18,17 +19,17 @@ session.sql(f"use warehouse {WAREHOUSE_NAME}").collect()
 
 root = Root(session)
 
-def truncate_table(session: Session, from_table: str, to_table: str, count) -> str:
+def truncate_table(session: Session) -> str:
     # Truncate a table
-    session.table(from_table).limit(count).write.save_as_table(to_table)
+    session.table("snowflake_sample_data.tpch_sf100.lineitem").limit(10).write.save_as_table("truncated_table", mode = "overwrite")
     return "Truncate table created successfully"
 
-def filter_by_shipmode(session: Session, shipmode: str) -> str:
-    session.table("snowflake_sample_data.tpch_sf100.lineitem").filter(col("L_SHIPMODE") == shipmode).write.save_as_table("filter_table")
+def filter_by_shipmode(session: Session) -> str:
+    session.table("snowflake_sample_data.tpch_sf100.lineitem").filter(col("L_SHIPMODE") == "AIR").write.save_as_table("filter_table", mode = "overwrite")
     return "Filter table successfully created!"
 
 
-tasks_stage = f"{DATABASE_NAME}.{SCHEMA_NAME}.tasks_stage"
+tasks_stage = f"{DATABASE_NAME}.{SCHEMA_NAME}.task_stage"
 
 task1 = Task(
     name="task_python_api_truncate",
@@ -38,7 +39,7 @@ task1 = Task(
         packages=["snowflake-snowpark-python"]
     ),
     warehouse=WAREHOUSE_NAME,
-    schedule=timedelta(minutes=1)
+    schedule=timedelta(minutes=2)
 )
 
 task2 = Task(
@@ -51,19 +52,25 @@ task2 = Task(
     warehouse=WAREHOUSE_NAME
 )
 
+task2.predecessors = [task1.name]
+
 # Create the tasks
 tasks = root.databases[DATABASE_NAME].schemas[SCHEMA_NAME].tasks
 truncate_task = tasks.create(task1, mode=CreateMode.or_replace)
-
-task2.predecessors = [truncate_task.name]
 filter_task = tasks.create(task2, mode=CreateMode.or_replace)
 
    
+
+
 truncate_task.resume()
+
+
 
 taskiter = tasks.iter()
 for t in taskiter:
     print(f"Task name: {t.name} | State : {t.state}")
+
+time.sleep(60)
 
 truncate_task.suspend()
 
